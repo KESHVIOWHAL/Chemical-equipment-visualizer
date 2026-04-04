4d4cf8f5969e91bc3511ded8ea1912d85389dc81
kanchanagaira <kanchangaira25@gmail.com>
2026-04-04 23:55:36 +0530

    feat: add equipment stats by type API

--- backend/equipment/urls.py
+++ backend/equipment/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report
 from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report, search_equipment
+from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report, search_equipment, equipment_stats_by_type
 urlpatterns = [
     path('upload/', upload_csv, name='upload_csv'),
     path('datasets/', get_datasets, name='get_datasets'),
     path('datasets/<int:dataset_id>/', get_dataset_detail, name='get_dataset_detail'),
-    path('datasets/<int:dataset_id>/pdf/', generate_pdf_report, name='generate_pdf_report'),
-path('search/', search_equipment, name='search_equipment'),]
+    path('datasets/<int:dataset_id>/pdf/', generate_pdf_report, name='generate_pdf_report'  ),
+path('search/', search_equipment, name='search_equipment'), 
+path('stats/', equipment_stats_by_type, name='equipment_stats_by_type'),]
