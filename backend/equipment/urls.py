ab6becab5f641ac3b34a497e2d1bee5288b1250d
Apurva Gunjal <apurvagunjal2005@gmail.com>
2026-04-06 13:51:27 +0530

    feat: add delete dataset api

--- backend/equipment/urls.py
+++ backend/equipment/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report
 from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report, search_equipment
-from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report, search_equipment, equipment_stats_by_type
+from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report, search_equipment, equipment_stats_by_type 
+from .views import upload_csv, get_datasets, get_dataset_detail, generate_pdf_report, search_equipment, equipment_stats_by_type, delete_dataset
 urlpatterns = [
     path('upload/', upload_csv, name='upload_csv'),
     path('datasets/', get_datasets, name='get_datasets'),
     path('datasets/<int:dataset_id>/', get_dataset_detail, name='get_dataset_detail'),
     path('datasets/<int:dataset_id>/pdf/', generate_pdf_report, name='generate_pdf_report'  ),
+    path('datasets/<int:dataset_id>/delete/', delete_dataset, name='delete_dataset'),
 path('search/', search_equipment, name='search_equipment'), 
 path('stats/', equipment_stats_by_type, name='equipment_stats_by_type'),]
